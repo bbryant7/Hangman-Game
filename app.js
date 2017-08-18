88c39fc5d1a4e8039cd897f0be495166b2914ab0
Bailey Bryant <bailey.bryant10@gmail.com>
2017-08-18 14:12:38 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+// // const expressValidator = require('express-validator');
+// const session = require('express-session');
+const app = express();
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+app.use(bodyParser.urlencoded({ extended: false}));
+// app.use(expressValidator());
+
+// app.use(session({
+//   secret: 'keyboard cat',
+//   resave: false,
+//   saveUninitialized: true
+// }))
+
+app.get('/', function(req, res){
+  res.render('hangman')
+})
+
+app.listen(3000, function() {
+  console.log('Successfully started express application!');
+})
---
+++ readme.md
@@ -0,0 +1,19 @@
+General:
+create views folder
+create public folder
+touch app.js folder
+touch mustache file
+install
+  - express
+  - mustache-express
+  - mustache
+  - body parser
+  - session
+  - validator
+
+
+APP. JS
+require all relevant modules
+include boiler plate code for all modules
+thingy at the bottom that defines local host
+render mustache page
