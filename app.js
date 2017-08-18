15397646f5ce7f02b7dd1e41d01576ea074891e2
Bailey Bryant <bailey.bryant10@gmail.com>
2017-08-18 15:00:41 -0500

    create post route for guess input

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
 // // const expressValidator = require('express-validator');
-// const session = require('express-session');
+const session = require('express-session');
 const app = express();
 
 app.engine('mustache', mustacheExpress());
@@ -11,14 +11,20 @@ app.set('view engine', 'mustache');
 app.use(bodyParser.urlencoded({ extended: false}));
 // app.use(expressValidator());
 
-// app.use(session({
-//   secret: 'keyboard cat',
-//   resave: false,
-//   saveUninitialized: true
-// }))
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.get('/', function(req, res){
   res.render('hangman')
+})
+
+app.post('/guess', function(req, res){
+  let letter = req.body.letter;
+  console.log(req.body.letter);
+  res.render('hangman', {letter:req.body.letter})
 })
 
 app.listen(3000, function() {
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-General:
+Set-up:
 create views folder
 create public folder
 touch app.js folder
@@ -11,9 +11,18 @@ install
   - session
   - validator
 
-
-APP. JS
 require all relevant modules
 include boiler plate code for all modules
-thingy at the bottom that defines local host
-render mustache page
+create html skeleton to render
+listen function on port 3000
+create get route and render mustache page
+TEST AND COMMIT
+
+Sudo Code:
+create data array of words
+create submit form for guessing game
+create post route for form
+generate words
+display underlines with right amount of letters
+display amount of guesses
+post guess to guesses and if correct, word display
