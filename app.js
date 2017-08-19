855765162a4ddf997791f8d418c1508af1f09212
Bailey Bryant <bailey.bryant10@gmail.com>
2017-08-19 14:13:55 -0500

    error message for letter input working

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
-// // const expressValidator = require('express-validator');
+const expressValidator = require('express-validator');
 const session = require('express-session');
 const app = express();
+const data = [{  word: "Banana"}]
+const blanks = [{blank: "_ _ _ _ _ _"}]
+let guessedWords = [];
+// VARIABLES ^^
 
+// BOILER PLATE
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
-app.use(bodyParser.urlencoded({ extended: false}));
-// app.use(expressValidator());
-
+app.use(bodyParser.urlencoded({
+  extended: false}));
+app.use(expressValidator());
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true
 }))
+// BOILER PLATE
 
-app.get('/', function(req, res){
-  res.render('hangman')
+app.get('/', function(req, res) {
+  res.render('hangman', {blanks})
+
 })
 
-app.post('/guess', function(req, res){
+app.post('/guess', function(req, res) {
+req.checkBody('letter', 'please guess a letter').notEmpty();
+
+let errors = req.validationErrors();
+ if (errors) {
+   console.log(errors)
+   res.render("hangman", {errors});
+ }
+ else {
   let letter = req.body.letter;
-  console.log(req.body.letter);
-  res.render('hangman', {letter:req.body.letter})
+  guessedWords.push(letter);
+  res.render('hangman', {guessedWords});
+}
 })
 
 app.listen(3000, function() {
--- readme.md
+++ readme.md
@@ -22,7 +22,13 @@ Sudo Code:
 create data array of words
 create submit form for guessing game
 create post route for form
-generate words
+***generate words
+-for each over array of words, display words
+render words
+count length of word, create empty array, push _ onto array
+render array
+
+guess a letter, if true add to display, add to guess array,
 display underlines with right amount of letters
 display amount of guesses
 post guess to guesses and if correct, word display
